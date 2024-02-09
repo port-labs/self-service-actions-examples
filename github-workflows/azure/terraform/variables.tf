33d05fd091be2a21df6a19b89c22d9c42a197788
hpal <ahosea15@gmail.com>
2024-02-09 15:07:31 +0000

    terraform helper files

---
+++ github-workflows/azure/terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "resource_group_name" {
+  type        = string
+  default     = "arete-resources"
+  description = "RG name in Azure"
+}
+
+variable "location" {
+  type        = string
+  default     = "westus2"
+  description = "RG location in Azure"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage Account name in Azure"
+  default     = "fortizo"
+}
+
+variable "port_run_id" {
+  type        = string
+  description = "The runID of the action run that created the entity"
+  default     = ""
+}
+
+variable "port_client_id" {
+  type        = string
+  description = "The Port client ID"
+}
+
+variable "port_client_secret" {
+  type        = string
+  description = "The Port client secret"
+}
+
+variable "resource_tags" {
+  type = any
+  default = {
+    Environment = "Production"
+    Department  = "Finance"
+  }
+}
